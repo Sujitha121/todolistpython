4117dfd7431c13e804f899c9f7c34bc0720f96ca
Sujitha R <sujitha.r@s.amity.edu>
2025-07-22 16:41:31 +0530

    C3

--- test.c
+++ test.c
@@ -4,3 +4,9 @@ def show_menu():
 	print("\n--- To-Do List Menu ---")
     print("1. Add Task")
     print("2. View Tasks")
+	print("3. Remove Task")
+    print("4. Exit")
+
+while True:
+    show_menu()
+    choice = input("Enter your choice (1-4): ")
